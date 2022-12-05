7fed0dc6ea3e103b21749af65a7589835fa16cfa
codesyariah122 <pujiermanto@gmail.com>
2022-12-05 07:12:09 +0700

    update pricing and list categories panel header

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-  ssr: false,
   loading: {
     color: "#01d28e",
     height: "11px",
