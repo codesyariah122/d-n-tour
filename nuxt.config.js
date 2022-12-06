c04af9d76de8efcbbd8709f92a7986ef742dc6fd
codesyariah122 <pujiermanto@gmail.com>
2022-12-07 00:40:42 +0700

    api server setup as middleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: "server",
   serverMiddleware: [
     {
       path: "api",
