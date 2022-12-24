0aeb1d03ec010895baa344f7d298318e11dbe8b1
developer.outsource <pujiermanto@gmail.com>
2022-12-25 06:36:01 +0700

    added google adsense

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
+  ssr: false,
   // mode: "universal",
   // ssr: true,
   // serverMiddleware: [
